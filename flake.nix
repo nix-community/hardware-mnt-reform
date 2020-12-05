7ac0d003f6eceda75147b0f109f6025e8a8bb168
Emery Hemingway <ehmry@posteo.net>
2020-12-05 18:20:57 +0100

    Migrate overrides from nixos-hardware to here

--- flake.nix
+++ flake.nix
@@ -1,20 +1,20 @@
 {
-  description = "Bootable NixOS image for the MNT Reform";
+  description =
+    "NixOS hardware configuration and bootable image for the MNT Reform";
 
-  inputs = {
-    nixpkgs.url =
-      "github:NixOS/nixpkgs/4684bb931179e6a1cf398491cc2df97c03aa963f";
-    nixos-hardware.url = "github:NixOS/nixos-hardware/reform";
-  };
+  inputs.nixpkgs.url =
+    "github:NixOS/nixpkgs/4684bb931179e6a1cf398491cc2df97c03aa963f";
 
-  outputs = { self, nixpkgs, nixos-hardware }:
+  outputs = { self, nixpkgs }:
     let
       installer = nixpkgs.lib.nixosSystem {
         system = "aarch64-linux";
-        modules =
-          [ nixos-hardware.nixosModules.mnt-reform-nitrogen8m ./installer.nix ];
+        modules = [ self.nixosModules.default ./installer.nix ];
       };
     in {
+
+      nixosModules.default =
+        import ./nixos-module.nix nixpkgs.legacyPackages.aarch64-linux;
 
       packages.aarch64-linux = {
         inherit (installer.config.system.build) kernel initialRamdisk sdImage;
