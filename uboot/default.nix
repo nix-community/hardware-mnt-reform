129f466daea4e66283eb96735afd83a8a311aafc
Mikhail Klementev <blame@dumpstack.io>
2022-04-27 19:47:36 +0000

    Update reform U-Boot to v3

--- uboot/default.nix
+++ uboot/default.nix
@@ -2,15 +2,15 @@
 
 buildUBoot rec {
   pname = "uboot-reform2-imx8mq";
-  version = "2020-06-01";
+  version = "v3";
   src = fetchgit {
     url = "https://source.mnt.re/reform/reform-boundary-uboot.git";
     rev = version;
-    sha256 = "0ychnwhisjqm0gzyz0nv9xynl9g114xmxpwz4vm0l5w6sc60jshw";
+    sha256 = "sha256-C2JjEz/uv6N08kmzyjhVS8TZKNNtfpRatjGhewEclOM=";
   };
   defconfig = "nitrogen8m_som_4g_defconfig";
   extraMeta.platforms = [ "aarch64-linux" ];
   filesToInstall = [ "flash.bin" ];
-  patches = [ ./shell-syntax.patch ./env_vars.patch ];
+  patches = [ ./env_vars.patch ];
   makeFlags = filesToInstall;
 }
