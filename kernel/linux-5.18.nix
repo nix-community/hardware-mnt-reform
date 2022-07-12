5725a547920f6d25dd22b3969cc80f581812fc14
Mikhail Klementev <blame@dumpstack.io>
2022-07-12 08:52:28 +0000

    linux: 5.18.9 -> 5.18.10

--- kernel/linux-5.18.nix
+++ kernel/linux-5.18.nix
@@ -3,7 +3,7 @@
 with lib;
 
 buildLinux (args // rec {
-  version = "5.18.9";
+  version = "5.18.10";
 
   # modDirVersion needs to be x.y.z, will automatically add .0 if needed
   modDirVersion = if (modDirVersionArg == null) then concatStringsSep "." (take 3 (splitVersion "${version}.0")) else modDirVersionArg;
@@ -13,6 +13,6 @@ buildLinux (args // rec {
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v5.x/linux-${version}.tar.xz";
-    sha256 = "sha256-OILib87c/jzPwVi5vi2V3yXybDeV7PGtlXCO1TL1yTw=";
+    sha256 = "sha256-+Z49Adu47m/nZdnLKy4HOv0txYHA05eXUchidF1z3uM=";
   };
 } // (args.argsOverride or {}))
