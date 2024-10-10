b6d01d44c67321f27a817a628a1dd7f2a1ca264f
Emery <ehmry@posteo.net>
2024-10-10 18:44:20 +0100

    fixup nixpkgs import

--- pocket.nix
+++ pocket.nix
@@ -1,34 +1,26 @@
 let
-  nixpkgs = import (
-    builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/5633bcff0c6162b9e4b5f1264264611e950c8ec7.tar.gz"
-  );
+  nixpkgs = builtins.fetchTarball
+    "https://github.com/NixOS/nixpkgs/archive/5633bcff0c6162b9e4b5f1264264611e950c8ec7.tar.gz";
   overlay = import ./overlay.nix;
-  pkgsFromX86 = nixpkgs {
+  pkgsFromX86 = import nixpkgs {
     localSystem = "x86_64-linux";
     crossSystem = "aarch64-linux";
     overlays = [ overlay ];
   };
-  pkgs = nixpkgs {
+  pkgs = import nixpkgs {
     localSystem = "aarch64-linux";
     crossSystem = "aarch64-linux";
-    overlays = [
-      (final: prev: { inherit pkgsFromX86; })
-      overlay
-    ];
+    overlays = [ (final: prev: { inherit pkgsFromX86; }) overlay ];
   };
-in
-rec {
+in rec {
   inherit pkgs;
 
   pocketModule = import ./nixos/pocket.nix;
 
-  installer = import "${<nixpkgs>}/nixos/lib/eval-config.nix" {
+  installer = import "${nixpkgs}/nixos/lib/eval-config.nix" {
     inherit pkgs;
     system = "aarch64-linux";
-    modules = [
-      ./nixos/pocket-installer.nix
-      pocketModule
-    ];
+    modules = [ ./nixos/pocket-installer.nix pocketModule ];
   };
 
   inherit (installer.config.system.build) kernel initialRamdisk sdImage;
